d9bfcaa3dd57940988892fd057ebadd6ab2b4416
pasanks <pasankavinga@gmail.com>
2022-10-27 10:13:10 +0530

    location field input validation / favourites table notification status badge

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
                                         Location
                                     </label>
                                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                           name="autocomplete" id="autocomplete" type="text" placeholder="Choose Location">
+                                           name="autocomplete" id="autocomplete" type="text" placeholder="Choose Location" required>
                                 </div>
                                 <div class="mb-6 hidden" >
                                     <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
--- resources/views/pages/favourites.blade.php
+++ resources/views/pages/favourites.blade.php
@@ -42,8 +42,13 @@
                                             {{$favourite->longitude}}
                                         </td>
                                         <td class="py-4 px-6">
-                                            {{$favourite->send_notification}}
+                                        @if($favourite->send_notification == true)
+                                             <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Enabled</span>
+                                        @else
+                                             <span class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">Disabled</span>
+                                        @endif
                                         </td>
+
                                         <td class="py-4 px-6">
                                             btn
                                         </td>
